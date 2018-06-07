8fb0fba9fc05fce5af50454febe2c5e24ebf7759
Iñaki <inaki.diaz@ikasle.egibide.org>
2018-06-07 21:44:44 +0200

    exercise 02

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,32 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class Emojifier {
+    public static void detectFaces(Context context, Bitmap bitmap) {
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Face> faces = detector.detect(frame);
+
+        for (int i = 0; i < faces.size(); ++i) {
+            Face face = faces.valueAt(i);
+            Log.d("detectFaces", face.toString());
+
+        }
+
+
+        detector.release();
+
+    }
+}
